91f4058d400feb88e83d65e1bf5b706616274d43
Michal Kober <michal.kober@etyka.cz>
2022-08-24 14:24:57 +0200

    init

---
+++ Tikaj-Api/src/main/java/cz/mkdev/tikajapi/domain/entity/Material.java
@@ -0,0 +1,75 @@
+package cz.mkdev.tikajapi.domain.entity;
+
+import cz.mkdev.tikajapi.domain.enums.UnitType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.ToString.Exclude;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table
+public class Material {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @Enumerated(EnumType.STRING)
+  private UnitType unit;
+
+  private BigDecimal unitPrice;
+
+  private String name;
+
+  private String description;
+
+  private LocalDateTime created;
+
+  private LocalDateTime edited;
+
+  @OneToMany(fetch = FetchType.LAZY)
+  @Exclude
+  private List<Image> imageList;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+      return false;
+    }
+    Material material = (Material) o;
+    return id != null && Objects.equals(id, material.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
+}
